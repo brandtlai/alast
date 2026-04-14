f3efcc85b93364c10121c58fa56405db881d4280
kaumi <kaumi@ss.kaumi.org>
2026-04-14 20:11:21 +0800

    feat: initialize alast frontend project scaffold

---
+++ frontend/src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div style={{ color: 'white', padding: '2rem' }}>ALAST Premier 2026</div>
+  </StrictMode>,
+)
